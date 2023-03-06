c547c85c402d65d25e4ea4e0003ce62fcb7660e5
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-03-06 14:28:27 -0300

    Feat: created page profile

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,52 @@
+import { FiCamera, FiUser, FiMail, FiLock, FiLogOut, FiChevronLeft } from 'react-icons/fi'
+import { Container, Form, Avatar } from './styles'
+
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { Footer } from '../../components/Footer'
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <button type='button'>
+          <FiChevronLeft size={32} />
+        </button>
+
+        <button className='logout' type='button'>
+          <FiLogOut size={25} />
+        </button>
+      </header>
+
+      <Form>
+        <Avatar>
+          <img src='http://github.com/palomarodrigs.png' alt='' />
+
+          <label htmlFor='avatar'>
+            <FiCamera />
+
+            <input id='avatar' type='file' />
+          </label>
+        </Avatar>
+
+        <Input
+          placeholder='Name'
+          type='text'
+          icon={FiUser}
+          // value={name}
+          // onChange={e => setName(e.target.value)}
+        />
+
+        <Input placeholder='Mail' type='text' icon={FiMail} />
+
+        <Input placeholder='Current password' type='password' icon={FiLock} />
+
+        <Input placeholder='New password' type='password' icon={FiLock} />
+
+        <Button title='Save' />
+      </Form>
+
+      <Footer />
+    </Container>
+  )
+}
---
+++ src/pages/Profile/styles.js
@@ -0,0 +1,92 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+
+  > header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    height: 144px;
+    padding: 0 123px;
+
+    background-color: ${({ theme }) => theme.COLORS.DARK_800};
+
+    button {
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    }
+  }
+
+  @media (max-width: 480px) {
+    > header {
+      padding: 0 27px 12px 27px;
+
+      svg {
+        width: 25px;
+      }
+
+      .logout {
+        svg {
+          width: 22px;
+        }
+      }
+    }
+  }
+`
+
+export const Form = styled.div`
+  width: 360px;
+  margin: 0 auto;
+
+  > div:nth-child(4) {
+    margin-top: 24px;
+  }
+`
+
+export const Avatar = styled.div`
+  position: relative;
+  width: 186px;
+  height: 186px;
+
+  margin: -84px auto 32px;
+
+  > img {
+    width: 186px;
+    height: 186px;
+
+    border: 1px solid ${({ theme }) => theme.COLORS.LIGHT_500};
+    border-radius: 50%;
+    object-fit: cover;
+  }
+
+  > label {
+    width: 48px;
+    height: 48px;
+    bottom: 7px;
+    right: 7px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+
+    cursor: pointer;
+
+    background-color: ${({ theme }) => theme.COLORS.RED_200};
+    border-radius: 50%;
+
+    input {
+      display: none;
+    }
+
+    svg {
+      width: 20px;
+      height: 20px;
+
+      color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    }
+  }
+`
