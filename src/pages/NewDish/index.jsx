fd2829496a0b5ab50592a8f75c7ec6d47a154ca9
Paloma Rodrigues Barbosa <palomarodrigues.0013@gmail.com>
2023-05-09 13:45:12 -0300

    Feat: create dish done

--- src/pages/NewDish/index.jsx
+++ src/pages/NewDish/index.jsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom'
 import { FiChevronLeft, FiUpload } from 'react-icons/fi'
 import { Container, Form } from './styles'
 
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 import { Input } from '../../components/Input'
 import { Header } from '../../components/Header'
 import { Button } from '../../components/Button'
@@ -10,7 +13,56 @@ import { Section } from '../../components/Section'
 import { Textarea } from '../../components/Textarea'
 import { IngredientItem } from '../../components/IngredientItem'
 
+import { api } from '../../services/api'
+
 export function NewDish() {
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('main')
+  const [ingredients, setIngredients] = useState([])
+  const [newIngredient, setNewIngredient] = useState('')
+  const [price, setPrice] = useState('')
+  const [description, setDescription] = useState('')
+  const [selectedFile, setSelectedFile] = useState(null)
+
+  const navigate = useNavigate()
+
+  function handleAddIngredient() {
+    setIngredients((prev) => [...prev, newIngredient])
+    setNewIngredient('')
+  }
+
+  function handleRemoveIngredient(deleted) {
+    setIngredients((prev) => prev.filter((ingredient) => ingredient !== deleted))
+  }
+
+  async function handleCreateNewDish() {
+    const dishData = {
+      title,
+      category,
+      ingredients,
+      price,
+      description
+    }
+
+    const formData = new FormData()
+    formData.append('image', selectedFile)
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }
+
+    await api.post('/dishes', dishData, formData, config)
+    alert('Dish created successfully!')
+    navigate(-1)
+  }
+
+  function handleFileChange(event) {
+    const file = event.target.files[0]
+    setSelectedFile(file)
+  }
+
   return (
     <Container>
       <Header />
@@ -28,20 +80,20 @@ export function NewDish() {
             <div className='upload-image'>
               <Section title='Dish image' />
               <label htmlFor='upload'>
-                Select image
+                {selectedFile ? selectedFile.name : 'Select image'}
                 <FiUpload />
-                <input id='upload' type='file' />
+                <input id='upload' type='file' onChange={handleFileChange} />
               </label>
             </div>
 
             <div className='name'>
               <Section title='Name' />
-              <Input placeholder='Ex.: Ceasar salad' />
+              <Input placeholder='Ex.: Ceasar salad' onChange={(e) => setTitle(e.target.value)} />
             </div>
 
             <div className='options'>
               <Section title='Category' />
-              <select name='type'>
+              <select name='type' value={category} onChange={(e) => setCategory(e.target.value)}>
                 <option value='main'>Main</option>
                 <option value='drinks'>Drinks</option>
                 <option value='desserts'>Desserts</option>
@@ -51,24 +103,40 @@ export function NewDish() {
             <div className='ingredients'>
               <Section title='Ingredients' />
               <div className='tags'>
-                <IngredientItem value='test' />
-                <IngredientItem placeholder='test' value='' isNew />
+                {ingredients.map((ingredient, index) => (
+                  <IngredientItem
+                    key={String(index)}
+                    value={ingredient}
+                    onClick={() => handleRemoveIngredient(ingredient)}
+                  />
+                ))}
+
+                <IngredientItem
+                  isNew
+                  placeholder='New'
+                  onChange={(e) => setNewIngredient(e.target.value)}
+                  value={newIngredient}
+                  onClick={handleAddIngredient}
+                />
               </div>
             </div>
 
             <div className='price'>
               <Section title='Price' />
-              <Input placeholder='$0' />
+              <Input placeholder='$0' onChange={(e) => setPrice(e.target.value)} />
             </div>
 
             <div className='description'>
               <Section title='Description' />
-              <Textarea placeholder='Briefly talk about the dish, its ingredients, and composition' />
+              <Textarea
+                placeholder='Briefly talk about the dish, its ingredients, and composition'
+                onChange={(e) => setDescription(e.target.value)}
+              />
             </div>
           </div>
 
           <div className='save-changes'>
-            <Button title='Save changes' />
+            <Button title='Save changes' onClick={handleCreateNewDish} />
           </div>
         </Form>
       </main>
